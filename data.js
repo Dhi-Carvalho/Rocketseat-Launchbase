fda0a11d4b483f8146706ec31b6dd7cf87258f9d
Diogo Santana <dhi.carvalho85@gmail.com>
2020-08-03 14:42:51 -0300

    data file

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "starter",
+        image: "/assets/starter.svg",
+        title: "Cursos 100% online e gratuitos para você entrar com o pé direito nas tecnologias mais desejadas do mercado.",
+        price: "GRATUITO",
+    },
+    {
+        id: "launchbase",
+        image: "/assets/launchbase.svg",
+        title: "O LaunchBase é um treinamento no formato de bootcamp online que tem duração de 8 semanas. Guiando o aluno para alcançar seus maiores objetivos como dev.",
+        price: "PAGO",
+    },
+    {
+        id: "gostack",
+        image: "/assets/gostack.svg",
+        title: "O GoStack é um treinamento online, prático e intensivo, no formato de bootcamp. No GoStack o aluno vai a fundo nas tecnologias NodeJS, ReactJS e React Native, do zero ao deploy.",
+        price: "PAGO",
+    }
+]
