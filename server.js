447ca67389f201264c10bbd5355ed073d793a421
Diogo Santana <dhi.carvalho85@gmail.com>
2020-08-14 21:02:56 -0300

    add page redirect

--- server.js
+++ server.js
@@ -22,6 +22,24 @@ server.get("/content", function (req, res) {
     return res.render("content", { items: courses })
 })
 
+server.get("/course", function (req, res) {
+    const id = req.query.id
+
+    const course = courses.find(function(course) {
+        return course.id == id
+    })
+    
+    if (!course) {
+        return res.render("not-found")
+    }
+
+    return res.render("course", { item: course })
+})
+
+server.use(function(req, res) {
+    res.status(404).render("not-found")
+});
+
 server.listen(5501, function () {
     console.log("Server is running")
 })
