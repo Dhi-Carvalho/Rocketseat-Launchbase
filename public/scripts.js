c20b3fbcc0d3e2525dbbe6cfb91ebf7836f9f57f
Diogo Santana <dhi.carvalho85@gmail.com>
2020-08-14 21:01:47 -0300

    modal exclusion

--- public/scripts.js
+++ public/scripts.js
@@ -4,12 +4,6 @@ const cards = document.querySelectorAll('.card');
 for (let card of cards) {
     card.addEventListener("click", function(){
         const siteId = card.getAttribute("id")
-        modalOverlay.classList.add("active")
-        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${siteId}`
+        window.location.href = `/course?id=${siteId}`
     })
 }
-
-document.querySelector(".close-modal").addEventListener("click", function(){
-    modalOverlay.classList.remove("active")
-})
-
